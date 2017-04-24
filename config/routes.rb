c20e28388d944b7b8a094cff836c21721e387a3f
Tan Cher Shen <chershen88@hotmail.com>
2017-04-24 22:16:37 +0800

    Add Basic Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   resources :users
 
   get  '/home', to: 'static_pages#home'
-  get  '/register', to: 'users#new'
-  
+  get  '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
