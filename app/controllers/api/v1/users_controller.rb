e4365766de6d5571904967395d342c109ea62c87
Tan Cher Shen <chershen88@hotmail.com>
2017-04-26 18:52:31 +0800

    Complete User API CRUD

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,23 +7,40 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
     @user = User.find(params[:id])
 
-    #render json: {
-    #  yey: 'hahah'
-    #}
-
-    render partial: 'api/v1/users/user'
+    render partial: 'api/v1/users/user', status: :ok
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render partial: 'api/v1/users/user', status: 201
+      render partial: 'api/v1/users/user', status: :created
     else
       render json: {
         errors: @user.errors.full_messages,
-      }, status: 422
+      }, status: 	:unprocessable_entity
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render partial: 'api/v1/users/user', status: :ok
+    else
+      render json: {
+        errors: @user.errors.full_messages,
+      }, status: 	:unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    render json: {
+      result: "User deleted",
+    }, status: 	:ok
 
   end
 
