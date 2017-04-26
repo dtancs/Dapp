e2eb6110173e25c755e058dd984b6b477ccd0325
Tan Cher Shen <chershen88@hotmail.com>
2017-04-26 18:01:32 +0800

    Add api for user create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::UsersController < Api::V1::BaseController
+
+  def index
+    @users = User.order(id: :asc)
+  end
+
   def show
     @user = User.find(params[:id])
 
@@ -7,6 +12,25 @@ class Api::V1::UsersController < Api::V1::BaseController
     #}
 
     render partial: 'api/v1/users/user'
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render partial: 'api/v1/users/user', status: 201
+    else
+      render json: {
+        errors: @user.errors.full_messages,
+      }, status: 422
+    end
 
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
